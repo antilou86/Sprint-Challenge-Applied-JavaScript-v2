29becbd0bcbc6cea4516862a8516d41b67b0956c
Lou <antilou86@gmail.com>
2019-07-12 12:03:54 -0400

    WIP: finished tabs component. working on cards next.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,18 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+let promise = axios.get('https://lambda-times-backend.herokuapp.com/topics').then((response) => {
+    console.log(response.data.topics);
+    const topics = response.data.topics;
+
+    topics.forEach( (topic) => {
+        let tab = document.createElement('div');
+        tab.classList.add('tab');
+        console.log(tab);
+        tab.textContent = topic;
+        
+        let targetElement = document.querySelector('.topics');
+        return targetElement.appendChild(tab);
+    });
+})
